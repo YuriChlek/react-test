95e0a20835b8f787bb6b8fd08bf68a21085c4cdb
YuriChlek <yurichlek@gmail.com>
2020-11-23 10:52:18 +0200

    Form validation was added.

--- src/components/MessageForm/MessageForm.js
+++ src/components/MessageForm/MessageForm.js
@@ -1,19 +1,40 @@
-import React from "react"
+import React, {useState} from "react"
 import is from 'is_js';
 import style from "./style.css"
 
 const MessageForm = (props) => {
     const classes = [style.form],
-          closeButton = [style.close, 'close'];
+        closeButton = [style.close, 'close'],
+        [name, isValidName] = useState([style.inputWrapper]),
+        [email, isValidEmail] = useState([style.inputWrapper]),
+        [message, isValidMessage] = useState([style.messageWrapper]);
+
 
     if (props.classes) {
         classes.push(props.classes);
     }
 
     const validForm = (event) => {
-        event.preventDefault();
-        if (event.target[0].value.length < 4 || !is.email(event.target[1].value)) {
-            console.log('Hi')
+        if (event.target[0].value.length < 4 || !is.email(event.target[1].value) || event.target[2].value.length < 30) {
+            event.preventDefault();
+        }
+        if (event.target[0].value.length < 4) {
+            isValidName([style.inputWrapper, style.notValid]);
+        }
+        if (!is.email(event.target[1].value)) {
+            isValidEmail([style.inputWrapper, style.notValid]);
+        }
+        if (event.target[2].value.length < 30) {
+            isValidMessage([style.messageWrapper, style.notValid]);
+        }
+        if (event.target[0].value.length >= 4) {
+            isValidName([style.inputWrapper]);
+        }
+        if (is.email(event.target[1].value)) {
+            isValidEmail([style.inputWrapper]);
+        }
+        if (event.target[2].value.length >= 30) {
+            isValidMessage([style.messageWrapper]);
         }
     }
 
@@ -21,13 +42,18 @@ const MessageForm = (props) => {
         <form className={classes.join(' ')} onSubmit={validForm}>
             {props.formTitle && <h3 className={style.formTitle}>{props.formTitle}</h3>}
             {props.close && <span className={closeButton.join(' ')}/>}
-            <div className={style.inputWrapper}>
+            <div className={name.join(' ')}>
+                <span className={style.errorMessage}>Enter correct name. Minimal length 4 symbols</span>
                 <input placeholder={"Enter your name"} type={"text"}/>
             </div>
-            <div className={style.inputWrapper}>
+            <div className={email.join(' ')}>
+                <span className={style.errorMessage}>Enter correct email</span>
                 <input placeholder={"Enter your email"} type={"email"}/>
             </div>
-            <textarea className={style.message} name="message" id=""/>
+            <div className={message.join(' ')}>
+                <span className={style.errorMessage}>Enter correct message. Minimal length 30 symbols.</span>
+                <textarea className={style.message} name="message" id=""/>
+            </div>
             <span className={style.buttonWrap}>
                     <button type={"submit"} className={style.button}>{"Send Message"}</button>
                 </span>
